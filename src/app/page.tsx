f68d904181d834850221efdbab5f61d4422b60b2
F-Loo <frenyloo138@gmail.com>
2025-05-14 10:28:28 -0400

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import { getCurrentSession } from "@/actions/auth";
 import { getAllProducts } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
+import SalesCampaignBanner from "./components/layout/SalesCampaignBanner";
+import ProductGrid from "./components/product/ProductGrid";
 
 const Home = async() => {
   const {user} = await getCurrentSession()
   const products = await getAllProducts()
   return (
    <div>
-    Home
+    <SalesCampaignBanner/>
+    <section className="container mx-auto px-4 py-8">
+      <ProductGrid products= {products}/>
+    </section>
    </div>
   );
 }
